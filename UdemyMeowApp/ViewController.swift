a0cdad8d65c2ca0a507a530f6bc4befe75dd6dbe
Imam Asari <tahzan.la10@gmail.com>
2018-04-23 17:19:58 +0700

    first commit

--- UdemyMeowApp/ViewController.swift
+++ UdemyMeowApp/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    var catSound : AVAudioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label.isHidden = true;
+        //button.imageView?.contentMode = .scaleAspectFill
+        let soundFile = Bundle.main.path(forResource: "Cat", ofType: "wav")
+        
+        do {
+            try catSound = AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundFile!))
+        }
+        catch {
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        catSound.play()
+        label.isHidden = false
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: false) {_ in
+            self.label.isHidden = true;
+        }
+        
+        //Timer.scheduledTimer(withTimeInterval: 2, repeats: false, block: {_ in self.label.isHidden = true})
+        
+        // timer that call function
+        //Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(hideLabel), userInfo: nil, repeats: false)
+    }
+    
+    @objc func hideLabel() {
+        label.isHidden = true
+    }
+    
 }
 
